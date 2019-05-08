24e41e3d710727cdb8f4941c2c56f205aa8b4fcd
jason <jasonqiao36@gmail.com>
2019-05-08 15:14:19 +0800

    增加 一等函数

---
+++ fluent-python/chap5_function/15_clip.py
@@ -0,0 +1,12 @@
+def clip(text: str, max_len: 'int> 0' = 80) -> str:
+    end = None
+    if len(text) > max_len:
+        space_before = text.rfind(' ', 0, max_len)
+        if space_before >= 0:
+            end = space_before
+        else:
+            space_after = text.rfind(' ', max_len)
+            end = space_after
+    if end is None:
+        end = len(text)
+    return text[:end].rstrip()
